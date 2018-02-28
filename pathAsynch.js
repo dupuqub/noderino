632e23df785d24bfde8ac88caf3b699fdfb8696c
dupuqub <dupuqub@gmail.com>
2018-02-28 15:15:31 -0300

    no outside code

--- pathAsynch.js
+++ pathAsynch.js
@@ -1,7 +1,4 @@
 
-//......................................................................................................................
-// OGGTHETROLL
-//
 var
 filer     = require('fs'),
 folder    = process.argv[2],
@@ -21,22 +18,4 @@ callback = (error, list) =>
 },
 
 readContent = filer.readdir(folder, callback)
-
-//......................................................................................................................
-// LEARNYOUNODE
-//
-var fs = require('fs')
-var path = require('path')
-
-var folder = process.argv[2]
-var ext = '.' + process.argv[3]
-
-fs.readdir(folder, function (err, files) {
-  if (err) return console.error(err)
-  files.forEach(function (file) {
-    if (path.extname(file) === ext) {
-      console.log(file)
-    }
-  })
-})
 
